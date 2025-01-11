e843c4a5ed6ac63f70ab7c7f087167d0b9146d2b
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-01-11 17:22:10 +0530

    will work tommo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ThingsMap from './ThingsMap'
+import ObjectsInReact from './ObjectsInReact'
 
-
-ReactDOM.render(<ThingsMap />, document.getElementById("root"));
+ReactDOM.render(<ObjectsInReact />, document.getElementById("root"));
