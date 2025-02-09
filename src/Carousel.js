6c885e7b1b404063d2842853d1eb2402fc0d8ede
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-02-10 01:07:48 +0530

    Carousel done

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,41 +1,46 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const Carousel = () => {
+    const imgArray = ["https://images.saymedia-content.com/.image/t_share/MTczOTM5NzMzODQyMzcxNjQ4/guts-a-berserk-character-analysis.jpg",
 
-    const [imgIndex, setImgIndex] = useState(0)
-    const data = [
-                "https://i.pinimg.com/236x/f0/c0/c3/f0c0c33933dd14df7335c44a3f15ed30.jpg",
-                "https://i.pinimg.com/1200x/23/5e/09/235e09099e71c062df1aea0d2babd2a6.jpg", 
-                "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a8169f7a-f0ab-4485-a9c8-2a6eb0a56662/delgo35-56bf3a81-468c-4f4e-96ea-dc868e73c37f.jpg/v1/fill/w_1280,h_890,q_75,strp/random_aesthetic_wallpaper___free_2_use__by_cocozart_delgo35-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODkwIiwicGF0aCI6IlwvZlwvYTgxNjlmN2EtZjBhYi00NDg1LWE5YzgtMmE2ZWIwYTU2NjYyXC9kZWxnbzM1LTU2YmYzYTgxLTQ2OGMtNGY0ZS05NmVhLWRjODY4ZTczYzM3Zi5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.o1w_3bui0ecVpqnArToFOabfJTZzqq6vC6mIAIEzyeo",
-
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp4PYOOovTF-KrqfH7WZsrwHUigANjdXfsoQ&s",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMv4kcTedtiZ9YZJxhkUcCjKHiuSZvOIVTGQ&s"
     ]
 
-    function prevImg(){
-        if(imgIndex === 0){
-            setImgIndex(data.length - 1)
+
+    const [imgIndex, setIndex] = React.useState(0)
+
+    const handleNext = function(){
+        if( imgIndex === imgArray.length - 1){
+            setIndex(0)
         }
-        else setImgIndex(function(index){
-            return index = index - 1
-        })
-    }
-    function nextImg(){
-        if(imgIndex === data.length - 1){
-            setImgIndex(0)
-        }
-        else setImgIndex(function(index){
+        else {
+            setIndex(function(index){
             return index = index + 1
         })
     }
+    }
+    const handlePrev = function(){
+        if( imgIndex === 0){
+            setIndex(imgArray.length - 1)
+        }
+        else {
+            setIndex(function(index){
+            return index = index - 1
+        })
+    }
+    }
 
-    const styling = {
-        height: "400px",
-        width: "400px"
+    const imgSize = {
+        width: "400px",
+        height: "400px"
     }
   return (
     <div>
-        <button onClick={prevImg}>Prev</button>
-        <img style = {styling} src={data[imgIndex]} />
-        <button onClick={nextImg}>Next</button>
+    <h3>WIll make a carousel here</h3>
+    <button onClick={handlePrev}>prev</button>
+    <img style={imgSize} src={imgArray[imgIndex]} />
+    <button onClick={handleNext}>next</button>
     </div>
   )
 }
